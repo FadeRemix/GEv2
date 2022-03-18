216d07b7c720ad464a20962962dad885959f143f
FadeRemix <zbk1103070@gmail.com>
2022-03-18 19:57:22 -0400

    fixed

--- GE-Script.lua
+++ GE-Script.lua
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
 local Wait = library.subs.Wait -- Only returns if the GUI has not been terminated. For 'while Wait() do' loops
 
@@ -144,6 +143,3 @@ PlayerDropDown = PlayerSeleSection:AddDropdown({
 	print(player)
 	end
 })
-=======
-
->>>>>>> 5787fdf3f55d392b8eced2d5cb0eaa0e93e6c720

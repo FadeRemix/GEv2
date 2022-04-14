eda4f1adaddbd9a07866bab7a4538f57cfc2060b
FadeRemix <64497456+FadeRemix@users.noreply.github.com>
2022-04-14 00:17:34 -0400

    -

--- GE-Script.lua
+++ GE-Script.lua
@@ -1,5 +1,4 @@
 local library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
-local Wait = library.subs.Wait -- Only returns if the GUI has not been terminated. For 'while Wait() do' loops
 ---------------------------------------------
 local PepsisWorld = library:CreateWindow({
 	Name = "General Exploit v2",
